dc2552b33280413a099f352f1bb493f257861a8c
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2022-10-17 19:51:21 +0300

    Add bold for dates in messages, remove bold from creation date

--- BiblionegaBot/Sender.cs
+++ BiblionegaBot/Sender.cs
@@ -6,11 +6,14 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
+using System.Text.RegularExpressions;
 
 namespace BiblionegaBot
 {
     public class Sender : ISender
     {
+        private static readonly Regex _boldRegex = new("(?:\\d{1,2}\\.\\d{1,2}\\.\\d{4}г?|\\d{2}:\\d{2}|\\d{1,2} (?:января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря)|(?:с|с|до|по) \\d{2}:\\d{2})");
+
         private readonly ILogger<Sender> _logger;
         private readonly ITelegramBotClient _botClient;
 
@@ -25,11 +28,26 @@ namespace BiblionegaBot
             return _botClient.GetMeAsync();
         }
 
+        private static string NormalizeMessage(string message)
+        {
+            message = message.Replace("&nbsp;", " ");
+            message = Regex.Replace(message, "\\s\\s+", " ");
+            message = Regex.Replace(message, ":{2,}", ":");
+            return message.Trim();
+        }
+
+        private static string BuildMessage(Anounce anounce)
+        {
+            var title = $"<b>{anounce.Category.GetDescription()} {anounce.Title}</b>";
+            var date = $"[{anounce.Created:dd.MM.yyyy HH:mm}]";
+            var message = _boldRegex.Replace(NormalizeMessage(anounce.Message), m => "<b>" + m.Value + "</b>");
+
+            return title + "\n" + date + "\n<i>" + message+"</i>";
+        }
+
         public async Task<Message> SendAnounceAsync(Anounce anounce, string chatId)
         {
-            var message = $"<b>{anounce.Category.GetDescription()} {anounce.Title}</b>\n" +
-                $"<b>{anounce.Created:dd.MM.yyyy HH:mm}</b>\n" +
-                $"<i>{anounce.Message}</i>";
+            var message = BuildMessage(anounce);
             try
             {
                 var result = await _botClient.SendTextMessageAsync(
